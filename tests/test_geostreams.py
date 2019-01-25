a6e3fdcccd11aed33a4c2bbaf699d06c6f076438
Luigi Marini <lmarini@illinois.edu>
2019-01-25 16:21:24 -0600

    Removed unnecessary test that was failing.

--- tests/test_geostreams.py
+++ tests/test_geostreams.py
@@ -22,12 +22,3 @@ def test_get_sensors(caplog, host, key):
     logging.info("%s sensors found", len(sensors))
     assert response.status_code == 200 and len(sensors) != 0
 
-
-def test_raise_for_status(caplog, host, key):
-    client = ClowderClient(host=host, key=key)
-    try:
-        client.get_json("this_path_does_not_exist")
-        assert False
-    except HTTPError as e:
-        logging.error(e.message)
-        assert True
