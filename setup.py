ed6e366c93b56485a5a87a8f7bdffc011b5cfdec
Diego Calderon <diegoac2@illinois.edu>
2020-03-05 12:08:20 -0600

    updated files to match the requirements of PyPi

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from distutils.core import setup
 
 setup(name='pygeotemporal',
       version='0.1.0',
+      url='https://opensource.ncsa.illinois.edu/bitbucket/projects/GEOD/repos/pygeotemporal/browse',
       packages=['pygeotemporal'],
       )
