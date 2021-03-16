21184084acd3e96831a97ad2c72da38c39c6f440
Max Burnette <mburnet2@illinois.edu>
2021-03-16 13:24:02 -0500

    version bump to 4.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pygeostreams',
-      version='0.2.0',
+      version='4.0.0',
       url='https://opensource.ncsa.illinois.edu/bitbucket/projects/GEOD/repos/pygeostreams/browse',
       packages=['pygeostreams'],
       )
